997d78b4fbe0faa15691be4d19967b9c843f03ae
syifa dharmawan <dharmawansyifa@gmail.com>
2023-11-15 14:28:18 +0700

    done tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_flutter/screens/list_product.dart';
 import 'package:shopping_list_flutter/screens/menu.dart';
 import 'package:shopping_list_flutter/screens/shoplist_form.dart';
 
@@ -61,6 +62,17 @@ class LeftDrawer extends StatelessWidget {
                     builder: (context) => ShopFormPage(),
                     ));
             },
+            ),
+            ListTile(
+            leading: const Icon(Icons.shopping_basket),
+            title: const Text('Daftar Produk'),
+            onTap: () {
+                // Route menu ke halaman produk
+                Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ProductPage()),
+                );
+              },
             ),
         ],
       ),
--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_list_flutter/screens/list_product.dart';
 import 'package:shopping_list_flutter/screens/shoplist_form.dart';
 
 class ShopItem {
@@ -15,11 +18,13 @@ class ShopCard extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
+      final request = context.watch <CookieRequest>();
         return Material(
         color: Colors.indigo,
         child: InkWell(
             // Area responsive terhadap sentuhan
-            onTap: () {
+            onTap: () 
+            async{
             // Memunculkan SnackBar ketika diklik
             ScaffoldMessenger.of(context)
                 ..hideCurrentSnackBar()
@@ -31,8 +36,29 @@ class ShopCard extends StatelessWidget {
                 context,
                 MaterialPageRoute(builder: (context) => ShopFormPage()),
               );
-
-            }
+            } else if (item.name == "Lihat Produk") {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const ProductPage()));
+            } else if (item.name == "Logout") {
+                final response = await request.logout(
+                    // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+                    "http://<http://syifa-mahira-tutorial.pbp.cs.ui.ac.id/>/auth/logout/");
+                String message = response["message"];
+                if (response['status']) {
+                  String uname = response["username"];
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("$message Sampai jumpa, $uname."),
+                  ));
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("$message"),
+                  ));
+                }
+              }
             },
             child: Container(
             // Container untuk menyimpan Icon dan Text
